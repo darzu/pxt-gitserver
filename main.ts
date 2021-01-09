a34bc9e4a1e6498505fd165289da473663ad309b
darzu <daryl.zuniga@gmail.com>
2021-01-09 10:50:02 -0800

    init code

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 namespace git.gitserver {
     export function initGitServer() {
-        console.log("Hello from local git server")
+        console.log("Hello from initGitServer")
     }
 }
+
+console.log("Hello from local git server")
 
 const trgOrigin = window.origin
 onmessage = function(e) {
